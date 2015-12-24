1d4360fb26fd4bd824d042ce66a4d01882ea8f8e
ShakyCode <jelinek@gmail.com>
2015-12-24 16:54:04 -0600

    Allow login/signup with username/email, unique usernames, add a navbar, conditional soup?

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,4 +22,21 @@ class User < ActiveRecord::Base
   has_many :spoofs
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validate :validate_username
+  attr_accessor :login
+
+  def self.find_for_database_authentication(warden_conditions)
+      conditions = warden_conditions.dup
+      if login = conditions.delete(:login)
+        where(conditions).where(["username = :value OR lower(email) = lower(:value)", { :value => login }]).first
+      else
+        where(conditions.to_h).first
+    end
+  end
+
+  def validate_username
+    if User.where(email: username).exists?
+      errors.add(:username, :invalid)
+    end
+  end
 end
