f57235199bf5e64bac0d85ffdff637b765344788
ShakyCode <jelinek@gmail.com>
2015-12-24 19:41:07 -0600

    Get a working sucker punch job firing random SMS

--- app/jobs/sms_job.rb
+++ app/jobs/sms_job.rb
@@ -5,8 +5,22 @@ class SmsJob
     ActiveRecord::Base.connection_pool.with_connection do
       spoof = Spoof.find(spoof_id)
       #do something
-      puts spoof.body
+      boot_twilio
       #do somethign more with twilio, update response state from API
+      sms = @client.messages.create(
+            from: Rails.application.secrets.twilio_numbers.sample,
+            to: spoof.number,
+            body: spoof.body)
+      #todo
+      #sleep 10
+      #spoof.status = sms.status
+      #spoof.save
     end
+  end
+
+  def boot_twilio
+    account_sid = Rails.application.secrets.twilio_sid
+    auth_token = Rails.application.secrets.twilio_token
+    @client = Twilio::REST::Client.new account_sid, auth_token
   end
 end
