aea0857b46e9deb247b8248b48d5de45791ac81f
ShakyCode <jelinek@gmail.com>
2015-12-26 09:18:56 -0600

    Quotes and title

--- app/helpers/spoofs_helper.rb
+++ app/helpers/spoofs_helper.rb
@@ -1,5 +1,5 @@
 module SpoofsHelper
   def random_quote
-    ["Look at all this crap you're sending!", "Why are you still here?", "Good luck with your date!"].sample
+    ["Look at all this crap you're sending!", "Why are you still here?", "If you haven't got anything nice to say about anybody, come sit next to me."].sample
   end
 end
