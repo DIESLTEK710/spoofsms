40093957f19fe7e73884b4e9487b75e5d3336648
ShakyCode <jelinek@gmail.com>
2015-12-29 15:25:19 -0600

    Adjust schedle

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,11 +1,9 @@
 current_path  = '/home/deploy/spoofsms/current'
-rails_env     = 'production'
-
 
 every 1.day, :at => '7:00 am' do
   command "cd #{current_path} && RAILS_ENV=production bundle exec rake user:confirm"
 end
 
-every 3.day, :at => '8:00 am' do
+every 3.days, :at => '8:00 am' do
   command "cd #{current_path} && RAILS_ENV=production bundle exec rake user:remind"
 end
