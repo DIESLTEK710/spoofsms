24d3ec80ada0886f43432557732f79aa1232aadf
ShakyCode <jelinek@gmail.com>
2016-01-01 08:20:46 -0600

    Timeoutable 60 min

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,7 +27,7 @@ class User < ActiveRecord::Base
   has_many :comments
   after_create :build_quotum
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :timeoutable
   validate :validate_username
   validates :first_name, :last_name, :presence => true
   scope :unconfirmed, -> {where confirmed_at: nil}
